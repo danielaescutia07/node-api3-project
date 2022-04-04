bb3c4fd6f497d4bf113f3ca3ab3de5d8210655a5
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-03 22:19:12 -0500

    uninstalled and installed old version of knex and pulled in server into index.js file

--- api/server.js
+++ api/server.js
@@ -3,8 +3,14 @@ const express = require('express');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+// server.use(logger);
+
+// const logger = (req, res) => {
+//   console.log(req.method)
+// };
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
