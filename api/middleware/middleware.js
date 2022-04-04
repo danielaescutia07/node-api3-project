c15d4c43933246f747aae33476f024880282d5fd
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-03 22:33:25 -0500

    logger middleware created and imported into server.js

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const method = req.method;
+  const url = req.originalUrl;
+  const timestamp = new Date().toLocaleString();
+  console.log(`${method} ${url} ${timestamp}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUserId Middleware');
+  next();
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUser Middleware');
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validatePost Middleware');
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
